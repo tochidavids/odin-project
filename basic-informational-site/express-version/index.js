f84c00cbf048e33667c7ed1b1a4b3d09c31a89bd
Tochi Davids <tochidavids18@gmail.com>
2024-11-01 21:30:21 +0000

    project completed

--- basic-informational-site/express-version/index.js
+++ basic-informational-site/express-version/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 
-// app.set("view engine", "ejs");
-
 app.get("/", (req, res) => {
 	res.sendFile("index.html", { root: ".." });
+});
+
+app.get("/:path", (req, res) => {
+	res.sendFile(`${req.params.path}.html`, { root: ".." }, err =>
+		res.sendFile("404.html", { root: ".." }),
+	);
 });
 
 app.listen(3000);
