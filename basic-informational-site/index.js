f043aadc5751562891582278af5c1d1e225d21f8
Tochi Davids <tochidavids18@gmail.com>
2024-11-01 13:18:58 +0000

    project completed

--- basic-informational-site/index.js
+++ basic-informational-site/index.js
@@ -4,18 +4,23 @@ const url = require("url");
 
 const server = http.createServer((req, res) => {
 	const q = url.parse(req.url, true);
-	fs.readFile(`.${q.pathname}.html`, (err, html) => {
-		if (err) {
+	console.log("pathname:", q.pathname);
+	fs.readFile(
+		`.${q.pathname == "/" ? "/index" : q.pathname}.html`,
+		(err, html) => {
+			if (err) {
+				console.error("error:", err);
+				res.setHeader("Content-Type", "text/html");
+				return res.end(
+					fs.readFileSync("404.html", (err, _404page) => {
+						return _404page;
+					}),
+				);
+			}
 			res.setHeader("Content-Type", "text/html");
-			return res.end(
-				fs.readFile("404.html", (err, _404page) => {
-					return _404page;
-				}),
-			);
-		}
-		res.setHeader("Content-Type", "text/html");
-		res.end(html);
-	});
+			res.end(html);
+		},
+	);
 });
 
 server.listen(8080);
